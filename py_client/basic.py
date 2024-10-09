f0c2a351e2b9004f6147dfb339d1d2afaf667d37
alokshandilya <alok.shandilya10@gmail.com>
2024-10-10 04:42:09 +0530

    test: test `/api/` endpoint with requests library

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,16 +1,8 @@
 import requests
 
-# endpoint = "https://api.github.com/users/alokshandilya"
-endpoint = "http://localhost:8000"
+endpoint = "http://localhost:8000/api"
 
 response = requests.get(endpoint)
 
-# HTTP Request -> HTML
-# REST API HTTP Request -> JSON
-# JavaScript Object Notation ~ Python Dictionary
-
-# print("response.json() : ", response.json(), end="\n\n")
-print("response.status_code : ", response.status_code, end="\n\n")
-print(response.text)  # HTML response as text unicode
-print("response.url : ", response.url, end="\n\n")
-print("response.request : ", response.request)
+print(response.json())
+print(response.status_code)
