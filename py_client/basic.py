cf74238f26c7467db95111372893d47200825960
alokshandilya <alok.shandilya10@gmail.com>
2024-10-10 03:28:04 +0530

    test(django): test HTTP response from localhost:8000

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,6 +1,7 @@
 import requests
 
-endpoint = "https://api.github.com/users/alokshandilya"
+# endpoint = "https://api.github.com/users/alokshandilya"
+endpoint = "http://localhost:8000"
 
 response = requests.get(endpoint)
 
@@ -8,8 +9,8 @@ response = requests.get(endpoint)
 # REST API HTTP Request -> JSON
 # JavaScript Object Notation ~ Python Dictionary
 
-print("response.json() : ", response.json(), end="\n\n")
+# print("response.json() : ", response.json(), end="\n\n")
 print("response.status_code : ", response.status_code, end="\n\n")
-# print(response.text") # HTML response as text unicode
+print(response.text)  # HTML response as text unicode
 print("response.url : ", response.url, end="\n\n")
 print("response.request : ", response.request)
