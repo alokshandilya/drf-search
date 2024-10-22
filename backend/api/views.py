0221b6074abebb0df075653b2e146950ffac657a
alokshandilya <alok.shandilya10@gmail.com>
2024-10-22 16:35:49 +0530

    docs(notes): how to echo GET data
    django HttpRequest instance: request (can be named anything)
    
    - request.GET: QueryDict of query parameters
    - request.headers: dict of headers, (need to be converted to dict)
    - request.content_type: content type of request body (application/json)
      - it's already in headers
    - request.body: byte string (b'') of JSON data
      - data = json.loads(request.body)
        - json.loads() raises exception if JSON is invalid
        - json.loads parses body (b'' byte string JSON) to dict
    - etc.

--- backend/api/views.py
+++ backend/api/views.py
@@ -11,16 +11,25 @@ def api_home(request, *args, **kwargs):
     first parameter of view is instance of HttpRequest typically named request
     print(dir(request))  # print all attributes and methods of request
     """
-    print(request.GET)  # dictionary of query parameters
-    body = request.body  # byte string (b'') of JSON data
     data = {}
     try:
-        data = json.loads(body)  # parse the body (JSON) -> dictionary
+        """
+        json.loads(request.body) parse body (b'' byte string JSON) to dict
+        json.loads() raises exception if JSON is invalid
+        """
+        data = json.loads(request.body)
     except Exception as e:
         print(e)
 
+    """
+    - request.GET is a QueryDict, dict of query parameters
+    - request.headers is a dict of headers
+    - request.content_type is content type of request body (application/json)
+      - it's already in headers
+    """
     data["params"] = dict(request.GET)
-    data["headers"] = dict(request.headers)
-    data["content_type"] = request.content_type
-    print(data)
+    # data["headers"] = dict(request.headers)
+    # data["content_type"] = request.content_type
+
+    print(data)  # will display JSON data and query parameters
     return JsonResponse({"message": "Hello, World!"})
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ endpoint = "http://localhost:8000/api/"
 
 response = requests.get(
     endpoint,
-    params={"product_id": 7},  # query parameters
+    params={"product_id": 7},  # query parameters (can be placed in endpoint)
     json={"username": "aloks"},  # JSON data in body
 )
 
