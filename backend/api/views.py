e1c0b4edf8042770db2774897e83d20cb138fd61
alokshandilya <alok.shandilya10@gmail.com>
2024-11-01 03:57:46 +0530

    docs: update notes on how to "echo GET data"

--- backend/api/views.py
+++ backend/api/views.py
@@ -22,10 +22,13 @@ def api_home(request, *args, **kwargs):
         print(e)
 
     """
-    - request.GET is a QueryDict, dict of query parameters
-    - request.headers is a dict of headers
-    - request.content_type is content type of request body (application/json)
-      - it's already in headers
+    - "request.GET" is used to access query parameters
+        - it's a QueryDict, dict of query parameters
+    - "request.headers" is used to access headers
+        - it's a dict of headers, previously (request.META)
+    - request.content_type is used to access content type of request body
+        - it's already in headers
+        - eg. (application/json)
     """
     data["params"] = dict(request.GET)
     # data["headers"] = dict(request.headers)
