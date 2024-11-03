0b3f43d145b65b8313a3cc1dcccd1c8b1973abf9
Alok Shandilya <alok.shandilya10@gmail.com>
2024-11-03 15:48:51 +0530

    feat: add Product model and random product API view
    - https://docs.djangoproject.com/en/5.1/ref/models/fields/
    - Add Product model with title, content, and price fields
    - Add random product API view to return a random product
      - Use Django's `order_by("?").first()` to get a random product
    - Register Product model in Django admin

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,38 +1,16 @@
-import json
-
 from django.http import JsonResponse
+from products.models import Product
 
 
 def api_home(request, *args, **kwargs):
-    """
-    API view for the home page (http://localhost:8000/api/)
+    random_product = Product.objects.order_by("?").first()
 
-    Notes:
-    first parameter of view is instance of HttpRequest typically named request
-    print(dir(request))  # print all attributes and methods of request
-    """
     data = {}
-    try:
-        """
-        json.loads(request.body) parse body (b'' byte string JSON) to dict
-        json.loads() raises exception if JSON is invalid
-        """
-        data = json.loads(request.body)
-    except Exception as e:
-        print(e)
 
-    """
-    - "request.GET" is used to access query parameters
-        - it's a QueryDict, dict of query parameters
-    - "request.headers" is used to access headers
-        - it's a dict of headers, previously (request.META)
-    - request.content_type is used to access content type of request body
-        - it's already in headers
-        - eg. (application/json)
-    """
-    data["params"] = dict(request.GET)
-    # data["headers"] = dict(request.headers)
-    # data["content_type"] = request.content_type
+    if random_product:
+        data["id"] = random_product.id
+        data["title"] = random_product.title
+        data["content"] = random_product.content
+        data["price"] = random_product.price
 
-    print(data)  # will display JSON data and query parameters
-    return JsonResponse({"message": "Hello, World!"})
+    return JsonResponse(data)
--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField(null=True, blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=7, default=149.99)
+
+    def __str__(self):
+        return f"{self.title} | Rs. {self.price}"
