117f9c8e65cbf1dad65eea16ce2a51223d502cb5
Alok Shandilya <alok.shandilya10@gmail.com>
2024-11-03 19:09:23 +0530

    docs: `@property` decorator in Django models, `@api_view` in DRF
    - `@property` decorator in Django model doesn't change the schema, doesn't
      add a column to the database, it's just a method that can be accessed
      as an attribute.
      - is not shown by `model_to_dict` method
    
    - `@api_view` decorator in DRF is used to create a view that responds to
      HTTP methods.
      - accepts a list of methods that the view should respond to (GET, POST,
        PUT, DELETE, etc.)

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,23 @@
 from django.forms.models import model_to_dict
-from django.http import JsonResponse
 from products.models import Product
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
     random_product = Product.objects.order_by("?").first()
     data = {}
 
     if random_product:
-        data = model_to_dict(random_product, fields=["id", "title", "price"])
+        data = model_to_dict(
+            random_product,
+            fields=[
+                "id",
+                "title",
+                "price",
+                "sale_price",  # model_to_dict doesn't show property
+            ],
+        )
 
-    return JsonResponse(data)
+    return Response(data)
--- backend/products/models.py
+++ backend/products/models.py
@@ -7,5 +7,10 @@ class Product(models.Model):
     content = models.TextField(null=True, blank=True)
     price = models.DecimalField(decimal_places=2, max_digits=7, default=149.99)
 
+    @property
+    def sale_price(self):
+        sale_price = float(self.price) * 0.8  # 20% off
+        return f"{sale_price:.2f}"
+
     def __str__(self):
         return f"{self.title} | Rs. {self.price}"
