85287785f708a9f81b959de4009e414408f28581
Alok Shandilya <alok.shandilya10@gmail.com>
2024-11-03 16:15:27 +0530

    refactor: use model_to_dict to serialize the product data
    - `model_to_dict` function in Django converts model instances into
    dictionaries.
    - `from django.forms import model_to_dict`
      - `from django.forms.models import model_to_dict`
    - `my_dict = model_to_dict(my_object, fields=['field1', 'field2'])`
      - `my_dict = model_to_dict(my_object, exclude=['field3'])`

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,13 @@
+from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from products.models import Product
 
 
 def api_home(request, *args, **kwargs):
     random_product = Product.objects.order_by("?").first()
-
     data = {}
 
     if random_product:
-        data["id"] = random_product.id
-        data["title"] = random_product.title
-        data["content"] = random_product.content
-        data["price"] = random_product.price
+        data = model_to_dict(random_product, fields=["id", "title", "price"])
 
     return JsonResponse(data)
