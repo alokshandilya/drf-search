3261ad2ab6104b6ac801c9ce951601af134dce3f
Alok Shandilya <alok.shandilya10@gmail.com>
2024-12-01 17:00:24 +0530

    refactor: use generic views

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
 from .views import (
-    # ProductDetailAPIView,
-    # ProductListCreateAPIView,
-    product_alt_view,
+    ProductDetailAPIView,
+    ProductListCreateAPIView,
+    # product_alt_view,
 )
 
 urlpatterns = [
-    # path("", ProductListCreateAPIView.as_view()),
-    path("", product_alt_view),
-    # path("<int:pk>/", ProductDetailAPIView.as_view()),
-    path("<int:pk>/", product_alt_view),
+    path("", ProductListCreateAPIView.as_view()),
+    # path("", product_alt_view),
+    path("<int:pk>/", ProductDetailAPIView.as_view()),
+    # path("<int:pk>/", product_alt_view),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,70 +1,71 @@
-from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
+# from django.shortcuts import get_object_or_404
+# from rest_framework.decorators import api_view
 
-# from rest_framework.generics import (
-#     ListCreateAPIView,
-#     RetrieveAPIView,
-# )
-from rest_framework.response import Response
-from rest_framework.views import status
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveAPIView,
+)
 
+# from rest_framework.response import Response
+# from rest_framework.views import status
 from .models import Product
 from .serializers import ProductSerializer
 
-# class ProductDetailAPIView(RetrieveAPIView):
-#     """
-#     RetrieveAPIView:
-#
-#     Used for read-only endpoints to represent a single model instance.
-#     Provides a `get` method handler.
-#     Extends: `GenericAPIView`, `RetrieveModelMixin`
-#     """
-#
-#     queryset = Product.objects.all()
-#     serializer_class = ProductSerializer
-#     # lookup_field = "pk"  # default
 
-
-# class ProductListCreateAPIView(ListCreateAPIView):
-#     """
-#     ListCreateAPIView:
-#
-#     Used for read-write endpoints to represent a collection of model instances.
-#     Provides `get` and `post` method handlers.
-#     Extends: `GenericAPIView`, `ListModelMixin`, `CreateModelMixin`
-#     """
-#
-#     queryset = Product.objects.all()
-#     serializer_class = ProductSerializer
-
-
-@api_view(["GET", "POST"])
-def product_alt_view(request, pk=None, *args, **kwargs):
+class ProductDetailAPIView(RetrieveAPIView):
     """
-    Function-based view to handle the same request as
-    `ProductListCreateAPIView`, `ProductDetailAPIView`
+    RetrieveAPIView:
+
+    Used for read-only endpoints to represent a single model instance.
+    Provides a `get` method handler.
+    Extends: `GenericAPIView`, `RetrieveModelMixin`
     """
-    method = request.method
 
-    if method == "GET":
-        if pk is not None:
-            # detail view
-            obj = get_object_or_404(Product, pk=pk)
-            serializer = ProductSerializer(obj, many=False)
-            return Response(serializer.data)
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # lookup_field = "pk"  # default
 
-        # list view
-        qs = Product.objects.all()
-        data = ProductSerializer(qs, many=True).data
-        return Response(data)
 
-    if method == "POST":
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+class ProductListCreateAPIView(ListCreateAPIView):
+    """
+    ListCreateAPIView:
 
-        return Response(
-            {"invalid": "not good data"},
-            status=status.HTTP_400_BAD_REQUEST,
-        )
+    Used for read-write endpoints to represent a collection of model instances.
+    Provides `get` and `post` method handlers.
+    Extends: `GenericAPIView`, `ListModelMixin`, `CreateModelMixin`
+    """
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+# @api_view(["GET", "POST"])
+# def product_alt_view(request, pk=None, *args, **kwargs):
+#     """
+#     Function-based view to handle the same request as
+#     `ProductListCreateAPIView`, `ProductDetailAPIView`
+#     """
+#     method = request.method
+#
+#     if method == "GET":
+#         if pk is not None:
+#             # detail view
+#             obj = get_object_or_404(Product, pk=pk)
+#             serializer = ProductSerializer(obj, many=False)
+#             return Response(serializer.data)
+#
+#         # list view
+#         qs = Product.objects.all()
+#         data = ProductSerializer(qs, many=True).data
+#         return Response(data)
+#
+#     if method == "POST":
+#         serializer = ProductSerializer(data=request.data)
+#         if serializer.is_valid(raise_exception=True):
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#
+#         return Response(
+#             {"invalid": "not good data"},
+#             status=status.HTTP_400_BAD_REQUEST,
+#         )
