ba6aa79fe2a20aad796e7386c7ece68e3bb167d3
Alok Shandilya <alok.shandilya10@gmail.com>
2024-12-01 16:50:13 +0530

    refactor: function based view for create, retrieve, list
    DRF provides various ways to create views for APIs, primarily through
    generic views and function-based views. Each approach has its strengths and
    weaknesses, making them suitable for different scenarios.
    
    Generic views are class-based views that simplify the creation of standard
    CRUD operations by providing built-in methods for common tasks. They abstract
    away repetitive code, allowing developers to focus on customizing behavior
    rather than boilerplate.
    
    Advantages of Generic Views:
    
      - Reduced Boilerplate:
        allow you to implement common patterns with minimal code. For instance,
        using `ListCreateAPIView` automatically handles GET and POST requests for a
        list of items without writing separate methods for each operation.
    
      - Reusability:
        promote code reuse through inheritance and mixins, enabling developers to
        create custom views by extending existing generic views.
    
      - Built-in Functionality:
        Generic views come with built-in support for pagination, filtering, and
        sorting, which can be easily configured.
    
    Function-based views (FBVs) are simpler and more explicit, allowing developers
    to define the logic of their API endpoints in a straightforward manner.
    They are ideal for cases where custom behavior is required that doesn't fit
    neatly into the CRUD model.
    
    Advantages of Function-Based Views:
    
      - Simplicity and Clarity:
        FBVs provide a clear structure where all logic is contained within a single
        function, making it easy to follow.
    
      - Customization:
        They allow for fine-grained control over request handling and response
        formatting. Developers can easily implement complex logic without the
        constraints of class-based structures.
    
      - Less Abstraction:
        Some developers prefer FBVs because they avoid the abstraction that comes
        with class-based views, making it easier to understand the flow of data.
    
    Choosing Between Generic Views and Function-Based Views:
    
    The choice between generic views and function-based views often depends on the
    specific requirements of the project:
    
    - Use Generic Views When:
      - You need to implement standard CRUD operations quickly.
      - You want to leverage built-in functionality like pagination and filtering.
      - You prefer a more structured approach that promotes reusability.
    
    - Use Function-Based Views When:
      - You require complex or highly customized logic that doesn't fit into
        standard CRUD operations.
      - You want explicit control over each part of the request/response cycle.
      - You prefer simplicity and clarity in your code structure.

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
 from .views import (
-    ProductDetailAPIView,
-    ProductListCreateAPIView,
+    # ProductDetailAPIView,
+    # ProductListCreateAPIView,
+    product_alt_view,
 )
 
 urlpatterns = [
-    path("", ProductListCreateAPIView.as_view()),
-    path("<int:pk>/", ProductDetailAPIView.as_view()),
+    # path("", ProductListCreateAPIView.as_view()),
+    path("", product_alt_view),
+    # path("<int:pk>/", ProductDetailAPIView.as_view()),
+    path("<int:pk>/", product_alt_view),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,36 +1,70 @@
-from rest_framework.generics import (
-    ListCreateAPIView,
-    RetrieveAPIView,
-)
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+
+# from rest_framework.generics import (
+#     ListCreateAPIView,
+#     RetrieveAPIView,
+# )
+from rest_framework.response import Response
+from rest_framework.views import status
 
 from .models import Product
 from .serializers import ProductSerializer
 
-# Create your views here.
+# class ProductDetailAPIView(RetrieveAPIView):
+#     """
+#     RetrieveAPIView:
+#
+#     Used for read-only endpoints to represent a single model instance.
+#     Provides a `get` method handler.
+#     Extends: `GenericAPIView`, `RetrieveModelMixin`
+#     """
+#
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     # lookup_field = "pk"  # default
 
 
-class ProductDetailAPIView(RetrieveAPIView):
+# class ProductListCreateAPIView(ListCreateAPIView):
+#     """
+#     ListCreateAPIView:
+#
+#     Used for read-write endpoints to represent a collection of model instances.
+#     Provides `get` and `post` method handlers.
+#     Extends: `GenericAPIView`, `ListModelMixin`, `CreateModelMixin`
+#     """
+#
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+
+
+@api_view(["GET", "POST"])
+def product_alt_view(request, pk=None, *args, **kwargs):
     """
-    RetrieveAPIView:
-
-    Used for read-only endpoints to represent a single model instance.
-    Provides a `get` method handler.
-    Extends: `GenericAPIView`, `RetrieveModelMixin`
+    Function-based view to handle the same request as
+    `ProductListCreateAPIView`, `ProductDetailAPIView`
     """
+    method = request.method
 
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    # lookup_field = "pk"  # default
+    if method == "GET":
+        if pk is not None:
+            # detail view
+            obj = get_object_or_404(Product, pk=pk)
+            serializer = ProductSerializer(obj, many=False)
+            return Response(serializer.data)
 
+        # list view
+        qs = Product.objects.all()
+        data = ProductSerializer(qs, many=True).data
+        return Response(data)
 
-class ProductListCreateAPIView(ListCreateAPIView):
-    """
-    ListCreateAPIView:
+    if method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    Used for read-write endpoints to represent a collection of model instances.
-    Provides `get` and `post` method handlers.
-    Extends: `GenericAPIView`, `ListModelMixin`, `CreateModelMixin`
-    """
-
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+        return Response(
+            {"invalid": "not good data"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
